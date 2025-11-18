02a87ffb4c2944c7896d25f6ec33b8e96eb5c8e1
mash3133 <maesha3133@gmail.com>
2025-11-18 17:12:25 -0500

    Refactor feature extraction process and add PCA dimensionality reduction

--- src/feature_extraction.py
+++ src/feature_extraction.py
@@ -22,11 +22,11 @@ def get_resnet_transform():
 def build_resnet_feature_extractor():
     model = models.resnet18(pretrained=True)
     model.fc = torch.nn.Identity() #remove last layer
-    model.eval()
+    model.eval() #not training
     return model
 
 def extract_resnet_features(dataset, model, transform):
-    with torch.no_grad():
+    with torch.no_grad(): #not training
         features_list = []
         labels_list = []
         for index in range(len(dataset)):
@@ -45,12 +45,13 @@ def extract_resnet_features(dataset, model, transform):
 transform = get_resnet_transform()
 model = build_resnet_feature_extractor()
 
+# X -> (5000,512)
+# y -> (5000)
+
 X_train_512, y_train = extract_resnet_features(train_balanced, model, transform)
 X_test_512, y_test = extract_resnet_features(test_balanced, model, transform)
 
+#2d numpy array, reduce 512 features to 50 using PCA
 pca = PCA(n_components=50)
 X_train_50 = pca.fit_transform(X_train_512)
 X_test_50 = pca.transform(X_test_512)
-
-print("Train features shape:", X_train_50.shape)   # (5000, 50)
-print("Test features shape:", X_test_50.shape)     # (1000, 50)
